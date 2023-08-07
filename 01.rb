7d8644b080c19f80ef9ab1ad654c775d58daba97
Ty <ty.j.kennedy@gmail.com>
2023-08-07 03:52:44 -0400

    Solved part 1 of Day 1

--- 01.rb
+++ 01.rb
@@ -1,2 +1,21 @@
 # frozen_string_literal: true
 
+def day_01(input_file)
+  numbers = [0]
+  File.foreach(input_file) do |line|
+    case line
+    when "\n"
+      numbers.append(0)
+    else
+      numbers[-1] = numbers.last + line.to_i
+    end
+
+  end
+
+  elves = numbers.map.with_index.to_a
+           .sort_by { | v, _ | -v }
+
+  elves[0]
+end
+
+puts day_01('inputs/01.txt')
