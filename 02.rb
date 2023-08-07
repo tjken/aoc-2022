84ba945264f2e54cc8339f39843397228ef61c89
Ty Kennedy <ty.j.kennedy@gmail.com>
2023-08-07 05:26:59 -0400

    WIP

--- 02.rb
+++ 02.rb
@@ -1,2 +1,11 @@
 # frozen_string_literal: true
 
+def day_02(input_file)
+  games =
+    File.readlines(input_file, chomp:true)
+        .map { |line| line.split(/ /)  }
+
+  games
+end
+
+p day_02('inputs/02_sample.txt')
