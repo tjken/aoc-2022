3a7cd47dd231bd63fa1b3eb48c46932517a1de61
Ty <ty.j.kennedy@gmail.com>
2023-08-07 05:54:29 -0400

    Solved part 1 of Day 2

--- 02.rb
+++ 02.rb
@@ -1,11 +1,35 @@
 # frozen_string_literal: true
 
-def day_02(input_file)
-  games =
-    File.readlines(input_file, chomp:true)
-        .map { |line| line.split(/ /)  }
+$scores = {
+  A: 1,
+  B: 2,
+  C: 3,
+  X: 1,
+  Y: 2,
+  Z: 3
+}
 
-  games
+$results = {
+  LOSS: 0,
+  DRAW: 3,
+  WIN: 6
+}
+def score(game)
+  $scores.fetch(game[1].to_sym) +
+    case ($scores.fetch(game[1].to_sym) - $scores.fetch(game[0].to_sym)) % 3
+    when 1
+      $results.fetch(:WIN)
+    when 0
+      $results.fetch(:DRAW)
+    when 2
+      $results.fetch(:LOSS)
+    end
 end
 
-p day_02('inputs/02_sample.txt')
+def day_02(input_file)
+  File.readlines(input_file, chomp:true)
+      .map { |line| line.split(/ /)  }
+      .map { |game| score(game) }
+end
+
+puts day_02('inputs/02.txt').sum
