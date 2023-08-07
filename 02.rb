5768ff501708a2200024624dc921a0b76cbc33eb
Ty <ty.j.kennedy@gmail.com>
2023-08-07 06:25:29 -0400

    Refactored to separate Part 1's score method to its own module.
    Encapsulated all logic apart from the puts statements into a 'Day02' module.

--- 02.rb
+++ 02.rb
@@ -1,35 +1,41 @@
 # frozen_string_literal: true
+module Day02
 
-$scores = {
-  A: 1,
-  B: 2,
-  C: 3,
-  X: 1,
-  Y: 2,
-  Z: 3
-}
+  $results = {
+    LOSS: 0,
+    DRAW: 3,
+    WIN: 6
+  }
 
-$results = {
-  LOSS: 0,
-  DRAW: 3,
-  WIN: 6
-}
-def score(game)
-  $scores.fetch(game[1].to_sym) +
-    case ($scores.fetch(game[1].to_sym) - $scores.fetch(game[0].to_sym)) % 3
-    when 1
-      $results.fetch(:WIN)
-    when 0
-      $results.fetch(:DRAW)
-    when 2
-      $results.fetch(:LOSS)
+  $scores = {
+    A: 1,
+    B: 2,
+    C: 3,
+    X: 1,
+    Y: 2,
+    Z: 3
+  }
+  module Part1
+
+    def self.score(game)
+      $scores.fetch(game[1].to_sym) +
+        case ($scores.fetch(game[1].to_sym) - $scores.fetch(game[0].to_sym)) % 3
+        when 1
+          $results.fetch(:WIN)
+        when 0
+          $results.fetch(:DRAW)
+        when 2
+          $results.fetch(:LOSS)
+        end
     end
+
+  end
+  def self.run(input_file, additional_method)
+    File.readlines(input_file, chomp:true)
+        .map { |line| line.split(/ /)  }
+        .map { |game| additional_method.call(game) }
+  end
+
 end
 
-def day_02(input_file)
-  File.readlines(input_file, chomp:true)
-      .map { |line| line.split(/ /)  }
-      .map { |game| score(game) }
-end
-
-puts day_02('inputs/02.txt').sum
+puts Day02.run('inputs/02.txt', Day02::Part1.method(:score)).sum
