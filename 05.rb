0ae7c526010eb71b7fc341d60fbefae684460949
Ty <ty.j.kennedy@gmail.com>
2023-08-14 00:50:28 -0400

    Added some file input ability. Able to read and create crate layouts. Moves are currently unimplemented.

--- 05.rb
+++ 05.rb
@@ -1,5 +1,35 @@
 # frozen_string_literal: true
 
-module Day05
-
+def read_input(input_file)
+  lines = File.read(input_file).split("\n")
+  crates = initialize_crates(lines.slice(0, lines.index('')).reverse)
+  moves = []
+  [crates, moves]
 end
+
+def read_from_lines(index, lines)
+  # TODO: fix bug where an empty space will be added to an output
+  # line_index is to find where a value for a specific crate is in a
+  # line, based on how the input file is written
+  line_index = (4 * index) + 1
+  output = []
+  lines.each do |line|
+    value = line[line_index]
+    output.push(value) unless value.nil? || value.empty?
+  end
+  output
+end
+
+def initialize_crates(crate_lines)
+  crates = []
+  (0...crate_lines.shift.split(' ').last.to_i).each do |index|
+    crates.push(read_from_lines(index, crate_lines))
+  end
+
+  crates
+end
+
+crates, moves = read_input('inputs/05_sample.txt')
+
+puts '--- Day 5: Supply Stacks ---'
+p crates, moves
